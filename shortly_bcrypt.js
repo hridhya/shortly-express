cdfc8699d9ffe157cd6d9d0dc9c74e8a65da0222
hridhya <menonhridhya@gmail.com>
2015-12-29 11:55:44 -0800

    Backup for bcrypt login

---
+++ shortly_bcrypt.js
@@ -0,0 +1,213 @@
+var express = require('express');
+var util = require('./lib/utility');
+var partials = require('express-partials');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var bcrypt = require('bcrypt');
+
+
+var db = require('./app/config');
+var Users = require('./app/collections/users');
+var User = require('./app/models/user');
+var Links = require('./app/collections/links');
+var Link = require('./app/models/link');
+var Click = require('./app/models/click');
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(partials());
+// Parse JSON (uniform resource locators)
+app.use(bodyParser.json());
+// Parse forms (signup/login)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Initialize session
+app.use(session(
+  { secret: 'hey'}
+));
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
+app.get('/',
+function(req, res) {
+  restrict(req, res, function(next) {
+    res.render('index');
+  });
+});
+
+app.get('/create',
+function(req, res) {
+  restrict(req, res, function(next) {
+    res.render('index');
+  });
+});
+
+app.get('/signup', 
+function(req, res) {
+  res.render('signup');
+});
+
+
+app.post('/signup',
+  function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    bcrypt.hash(password, '$2a$10$somethingheretobeasalt', function(err, result) {
+      if (err) {
+        console.log('err: ', err);      
+      } else {
+        console.log('create result: ', result);
+
+        new User({
+          'username': username,
+          'password': result
+        }).save().then(function(){
+          // console.log('success: ');
+          res.redirect('/');
+          res.send(200);
+        });
+
+      }
+    });
+});
+
+app.get('/login', 
+function(req, res) {
+  res.render('login');
+});
+
+app.post('/login',
+  function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    if(req.body.username && req.body.password) {
+
+
+      new User({'username': username})
+        .fetch()
+        .then(function(model) {
+          //console.log('username', model);
+          //console.log('password', model);
+          if(model === null) {
+            res.redirect('/login');
+          }
+
+          //var salt = '$2a$10$' + model.get('created_at');
+          bcrypt.hash(password, '$2a$10$somethingheretobeasalt', function(err, hash) {
+            //console.log('hash',hash);
+            //console.log('from db', model.get('password'));
+
+            if(hash === model.get('password')){
+              req.session.regenerate(function(){
+              req.session.user = req.body.username;
+              res.redirect('/');
+              });
+            } else{
+              res.redirect('/login');
+            }
+
+          });
+          
+        });
+    }
+    else {
+       res.redirect('/login');
+    }
+});
+
+app.get('/logout', function(request, response){
+    request.session.destroy(function(){
+        response.redirect('/login');
+    });
+});
+
+app.get('/links', 
+function(req, res) {
+  restrict(req, res, function(next) {
+    Links.reset().fetch().then(function(links) {
+      res.send(200, links.models);
+    });
+  });
+});
+
+app.post('/links', 
+function(req, res) {
+
+  var uri = req.body.url;
+  // uri = 'https://roflzoo.com/';
+  //console.log(uri);
+
+
+  if (!util.isValidUrl(uri)) {
+    console.log('Not a valid url: ', uri);
+    return res.send(404);
+  }
+
+  new Link({ url: uri }).fetch().then(function(found) {
+    if (found) {
+      //console.log('found: ', found);
+      res.send(200, found.attributes);
+    } else {
+      util.getUrlTitle(uri, function(err, title) {
+        if (err) {
+          console.log('Error reading URL heading: ', err);
+          return res.send(404);
+        }
+
+        Links.create({
+          url: uri,
+          title: title,
+          base_url: req.headers.origin
+        })
+        .then(function(newLink) {
+          res.send(200, newLink);
+        });
+      });
+    }
+  });
+});
+
+/************************************************************/
+// Write your authentication routes here
+/************************************************************/
+
+
+
+/************************************************************/
+// Handle the wildcard route last - if all other routes fail
+// assume the route is a short code and try and handle it here.
+// If the short-code doesn't exist, send the user to '/'
+/************************************************************/
+
+app.get('/*', function(req, res) {
+  new Link({ code: req.params[0] }).fetch().then(function(link) {
+    if (!link) {
+      res.redirect('/');
+    } else {
+      var click = new Click({
+        link_id: link.get('id')
+      });
+
+      click.save().then(function() {
+        link.set('visits', link.get('visits')+1);
+        link.save().then(function() {
+          return res.redirect(link.get('url'));
+        });
+      });
+    }
+  });
+});
+
+console.log('Shortly is listening on 4568');
+app.listen(4568);
